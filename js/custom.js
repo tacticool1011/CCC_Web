b72ad02ca07afe5117d0cbc8e8d3ac019f2eede4
Aaron <35825663+5up3rN00b@users.noreply.github.com>
2020-07-23 12:24:11 -0700

    Finished login/register

--- js/custom.js
+++ js/custom.js
@@ -12,14 +12,16 @@
     $('.navbar-collapse a').on('click',function(){
       $(".navbar-collapse").collapse('hide');
     });
+	
+	$(".navbar-fixed-top").addClass("top-nav-collapse");
 
-    $(window).scroll(function() {
-      if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-          } else {
-            $(".navbar-fixed-top").removeClass("top-nav-collapse");
-          }
-    });
+    //$(window).scroll(function() {
+    //  if ($(".navbar").offset().top > 50) {
+    //    $(".navbar-fixed-top").addClass("top-nav-collapse");
+    //      } else {
+    //        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    //      }
+    //});
 
 
     // SLIDER
